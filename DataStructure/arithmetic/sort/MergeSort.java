90b7f19e6f7484c8d52889812cfc12ba6317926b
zhangke <zhangke@95190.com>
2018-06-28 13:48:48 +0800

    算法

--- DataStructure/arithmetic/sort/MergeSort.java
+++ DataStructure/arithmetic/sort/MergeSort.java
@@ -2,10 +2,14 @@ package sort;
 
 /**
  * 归并排序
- * <p>
- * 1、将一个序列平均分为两个子序列，对这两个子序列进行排序 2、将两个子序列排序后在合并成同样一个序列，
- * <p>
- * 通过递归的方式，重复上面的过程，完成整个排序
+ *
+ * 时间复杂度：O(n*logn)
+ *
+ * 算法步骤：
+ * 1、将一个序列平均分为两个子序列，对这两个子序列进行排序
+ * 2、将两个子序列排序后在合并成同样一个序列，
+ * 3、通过递归的方式，重复上面的过程，完成整个排序
+ *
  */
 public class MergeSort extends Sort {
     public MergeSort() {
@@ -18,58 +22,51 @@ public class MergeSort extends Sort {
         return mergeSort(array, 0, array.length - 1);
     }
 
-    private int[] mergeSort(int[] array, int left, int right) {
+    public int[] mergeSort(int[] array, int left, int right) {
         if (left < right) {
             int middle = (left + right) / 2;
             mergeSort(array, left, middle);
             mergeSort(array, middle + 1, right);
-            // 合并
+
             merge(array, left, middle, right);
         }
         return array;
     }
 
-    /**
-     * 合并两个序列
-     *
-     * @param array
-     * @param left
-     * @param middle
-     * @param right
-     */
     private void merge(int[] array, int left, int middle, int right) {
-        int index = left;
-
-        // 创建一个临时数组，用于存放合并是的序列
         int[] tmpArray = new int[array.length];
         int tmpIndex = left;
-        int rightCursor = middle + 1;
 
-        //将序列的一边完整放入到数组中
-        while (left <= middle && rightCursor <= right) {
+        // 两个合并数组的起始位置下标
+        int firstCursor = left;
+        int secondCursor = middle + 1;
 
-            if (array[left] <= array[rightCursor]) {
-                // 如果左边的值小，那么左边的先加入到临时数组
-                tmpArray[tmpIndex++] = array[left++];
+        // 按照大小顺序将元素添加到临时数组中
+        while (firstCursor <= middle && secondCursor <= right) {
+            if (array[firstCursor] < array[secondCursor]) {
+                tmpArray[tmpIndex++] = array[firstCursor++];
             } else {
-                tmpArray[tmpIndex++] = array[rightCursor++];
+                tmpArray[tmpIndex++] = array[secondCursor++];
             }
         }
 
-        // 如果左边序列还存在元素没有放入，那么将剩余的放入
-        while (left <= middle) {
-            tmpArray[tmpIndex++] = array[left++];
+        // 第一个数组数据没有添加完全
+        while (firstCursor <= middle) {
+            tmpArray[tmpIndex++] = array[firstCursor++];
         }
 
-        // 如果右边序列还存在元素没有放入，那么将剩余的放入
-        while (rightCursor <= right) {
-            tmpArray[tmpIndex] = array[rightCursor++];
+        // 第二个数组没有添加完全
+        while (secondCursor <= right) {
+            tmpArray[tmpIndex++] = array[secondCursor++];
         }
 
-        // 将左右两个序列按照顺序合并到临时数组后，在将临时数组的值赋值给原数组中，这样对原数组该部分的序列完成了排序
-        while (index <= right) {
-            array[index] = tmpArray[index++];
+        // 两个数组按照顺组添加到临时数组后，将原来数组该段重新赋值为新数组
+        while (left <= right) {
+            array[left] = tmpArray[left];
+            left++;
         }
+
     }
+
 
 }
